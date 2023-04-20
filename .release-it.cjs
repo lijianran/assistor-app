af4f0ea81fd9ffb58dc87dbc17cee459f472517e
lijianran <lijianran@outlook.com>
2023-04-21 00:46:05 +0800

    chore: 更新图标，新增github updater地址

--- .release-it.cjs
+++ .release-it.cjs
@@ -1,24 +1,23 @@
 module.exports = {
-    git: {
-      commitMessage: 'v${version}',
-      tagName: 'v${version}'
+  git: {
+    commitMessage: "v${version}",
+    tagName: "v${version}",
+  },
+  github: {
+    release: false,
+    releaseName: "v${version}",
+  },
+  npm: {
+    publish: false,
+  },
+  hooks: {
+    "after:bump": "npm run update:version",
+  },
+  plugins: {
+    "@release-it/conventional-changelog": {
+      preset: "angular",
+      infile: "CHANGELOG.md",
+      ignoreRecommendedBump: true,
     },
-    github: {
-      release: false,
-      releaseName: 'v${version}'
-    },
-    npm: {
-      publish: false
-    },
-    hooks: {
-      'after:bump': 'npm run update:version'
-    },
-    plugins: {
-      '@release-it/conventional-changelog': {
-        preset: 'angular',
-        infile: 'CHANGELOG.md',
-        ignoreRecommendedBump: true
-      }
-    }
-  }
-  
+  },
+};
