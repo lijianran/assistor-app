3c9b8baa03572c2d16c35c3ea80ce02672ecd237
lijianran <lijianran@outlook.com>
2023-04-20 23:19:34 +0800

    ci: add release-it

---
+++ .release-it.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+    git: {
+      commitMessage: 'v${version}',
+      tagName: 'v${version}'
+    },
+    github: {
+      release: false,
+      releaseName: 'v${version}'
+    },
+    npm: {
+      publish: false
+    },
+    hooks: {
+      'after:bump': 'npm run update:version'
+    },
+    plugins: {
+      '@release-it/conventional-changelog': {
+        preset: 'angular',
+        infile: 'CHANGELOG.md',
+        ignoreRecommendedBump: true
+      }
+    }
+  }
+  
---
+++ scripts/update-version.cjs
@@ -0,0 +1,38 @@
+// 更新 cargo readme 中的版本号
+
+const { readFileSync, writeFileSync } = require('fs')
+const { resolve } = require('path')
+const { name, version } = require('../package.json')
+
+const updateReadmeVersion = () => {
+  const dir = resolve(__dirname, '../README.md')
+
+  let content = readFileSync(dir, 'utf-8')
+
+  const reg1 = /v\d+\.\d+\.\d+/g
+  const reg2 = /\d+\.\d+\.\d+/g
+
+  content = content.replace(reg1, `v${version}`).replace(reg2, version)
+
+  writeFileSync(dir, content)
+}
+
+const updateCargoVersion = () => {
+  const tomlDir = resolve(__dirname, '../src-tauri/Cargo.toml')
+  const lockDir = resolve(__dirname, '../src-tauri/Cargo.lock')
+
+  for (const dir of [tomlDir, lockDir]) {
+    let content = readFileSync(dir, 'utf-8')
+
+    const reg = new RegExp(
+      `name\\s*=\\s*"${name}"\\s*version\\s*=\\s*"\\d+.\\d+.\\d+"`
+    )
+
+    content = content.replace(reg, `name = "${name}"\nversion = "${version}"`)
+
+    writeFileSync(dir, content)
+  }
+}
+
+updateReadmeVersion()
+updateCargoVersion()
