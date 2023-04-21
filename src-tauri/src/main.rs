a92ce6e63da804d7b02557396509a5c55d50f823
lijianran <lijianran@outlook.com>
2023-04-21 17:50:37 +0800

    feat: 增加单例模式，更新软件图标

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,18 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
 
+use tauri::Manager;
+
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .plugin(tauri_plugin_single_instance::init(|app, _argv, _cwd| {
+            let window = app.get_window("main").unwrap();
+            window.show().unwrap();
+            window.unminimize().unwrap();
+            window.set_focus().unwrap();
+        }))
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
